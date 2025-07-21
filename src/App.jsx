bde96ff446330a26158d4406757eac4b04bfc285
James Lannon <jameslannon@Jamess-MacBook-Pro.local>
2025-07-21 12:59:47 -0700

    feat: add data.js with CORE_CONCEPTS object, use to populate CoreConcept components in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import reactImg from './assets/react-core-concepts.png';
+import { CORE_CONCEPTS } from './data.js';
 
 const reactDescriptions = ['Fundamental', 'Crucial', 'Core']
 
@@ -9,6 +10,7 @@ function genRandomInt(max) {
 function Header() {
 
   const description = reactDescriptions[genRandomInt(2)];
+
   return (
     <header>
       <img src={reactImg} alt="Stylized atom" />
@@ -21,11 +23,33 @@ function Header() {
   );
 }
 
+function CoreConcept(props) {
+
+  return (
+    <li>
+      <img src={props.image} alt={props.title} />
+      <h3>{props.title}</h3>
+      <p>
+        {props.description}
+      </p>
+    </li>
+  );
+}
+
 function App() {
   return (
     <div>
       <Header />
       <main>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcept title={CORE_CONCEPTS[0].title} description={CORE_CONCEPTS[0].description} image={CORE_CONCEPTS[0].image}/>
+            <CoreConcept title={CORE_CONCEPTS[1].title} description={CORE_CONCEPTS[1].description} image={CORE_CONCEPTS[1].image}/>
+            <CoreConcept title={CORE_CONCEPTS[2].title} description={CORE_CONCEPTS[2].description} image={CORE_CONCEPTS[2].image}/>
+            <CoreConcept title={CORE_CONCEPTS[3].title} description={CORE_CONCEPTS[3].description} image={CORE_CONCEPTS[3].image}/>
+            </ul>
+          </section>
         <h2>Time to get started!</h2>
       </main>
     </div>
