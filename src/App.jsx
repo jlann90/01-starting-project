e14cc7342c43094901b064fbe71502bfb863e0fe
James Lannon <jameslannon@Jamess-MacBook-Pro.local>
2025-07-21 14:54:51 -0700

    refactor: update CoreConcept component so props are destructured, Update first 3 uses of CoreConcept component in App component so they use spread operator and reduce amount of code

--- src/App.jsx
+++ src/App.jsx
@@ -23,14 +23,14 @@ function Header() {
   );
 }
 
-function CoreConcept(props) {
+function CoreConcept({image, title, description}) {
 
   return (
     <li>
-      <img src={props.image} alt={props.title} />
-      <h3>{props.title}</h3>
+      <img src={image} alt={title} />
+      <h3>{title}</h3>
       <p>
-        {props.description}
+        {description}
       </p>
     </li>
   );
@@ -44,9 +44,9 @@ function App() {
         <section id="core-concepts">
           <h2>Core Concepts</h2>
           <ul>
-            <CoreConcept title={CORE_CONCEPTS[0].title} description={CORE_CONCEPTS[0].description} image={CORE_CONCEPTS[0].image}/>
-            <CoreConcept title={CORE_CONCEPTS[1].title} description={CORE_CONCEPTS[1].description} image={CORE_CONCEPTS[1].image}/>
-            <CoreConcept title={CORE_CONCEPTS[2].title} description={CORE_CONCEPTS[2].description} image={CORE_CONCEPTS[2].image}/>
+            <CoreConcept {...CORE_CONCEPTS[0]}/>
+            <CoreConcept {...CORE_CONCEPTS[1]}/>
+            <CoreConcept {...CORE_CONCEPTS[2]}/>
             <CoreConcept title={CORE_CONCEPTS[3].title} description={CORE_CONCEPTS[3].description} image={CORE_CONCEPTS[3].image}/>
             </ul>
           </section>
