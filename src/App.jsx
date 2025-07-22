0f1aaed60f8cb50b2896450bc7e7a078fdddaa28
James Lannon <jameslannon@Jamess-MacBook-Pro.local>
2025-07-22 14:05:30 -0700

    feat: add setSelectedTopic function to handle state

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
+import { use, useState } from 'react';
 import { CORE_CONCEPTS } from './data.js';
 import Header from './components/Header/Header.jsx';
 import CoreConcept from './components/CoreConcept.jsx';
 import TabButton from './components/TabButton.jsx';
 
 function App() {
+  const [ selectedTopic, setSelectedTopic ] = useState('Please click a button');
+
   function handleSelect(selectedButton) { 
-    console.log(selectedButton)
+    setSelectedTopic(selectedButton);
+    console.log(selectedTopic)
   }
+
+  console.log('App component rendered');
 
   return (
     <div>
@@ -29,6 +35,7 @@ function App() {
             <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
             <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
           </menu>
+          {selectedTopic}
         </section>
       </main>
     </div>
